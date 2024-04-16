924a575c364920443c6cc3ca8557fd0e43c52e81
naadhira <hendy.appleton@hazelcast.com>
2024-04-16 10:39:39 -0700

    Docker compose 4 testing

--- Client/src/main/java/Client4CP.java
+++ Client/src/main/java/Client4CP.java
@@ -3,25 +3,26 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.cp.CPMap;
 
-import java.util.Map;
-
-public final class Client4P {
+public final class Client4CP {
     public static void main(String[] args) throws Exception {
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress("127.0.0.1");
         clientConfig.getNetworkConfig().setSmartRouting(false);
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-        Map<String, String> map = client.getMap("map");
-        map.put("1", "Tokyo");
-        map.put("2", "Paris");
-        map.put("3", "New York");
-        System.out.println("Finished loading map");
 
-        CPMap<String, String> cpMap = client.getCPSubsystem().getMap("cp-map");
-        cpMap.set("1", "Japan");
-        cpMap.set("2", "France");
-        cpMap.set("3", "USA");
+        CPMap<String, String> trektng = client.getCPSubsystem().getMap("trektng");
+        trektng.set("1", "Jean-Luc Picard");
+        trektng.set("2", "William Riker");
+        trektng.set("3", "Data");
+        trektng.set("4", "Beverly Crusher");
+        trektng.set("5", "Deanna Troi");
+        trektng.set("6", "Tasha Yar");
+        trektng.set("7", "Worf son of Mogh");
+        trektng.set("8", "Wesley Crusher");
+
+        //System.out.println("They killed off" + trektng.get("6") + "in season one." );
+
 
         HazelcastClient.shutdownAll();
     }
